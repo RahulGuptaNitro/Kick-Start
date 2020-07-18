d9e8ce42cf2a6b1d76e3a69430cf6437d95d6c3a
Rahul Kumar Gupta <rahulguptanitro@gmail.com>
2020-07-18 13:49:38 +0530

    Create 2020_Round-B_Bus_Routes.py

---
+++ 2020_Round-B_Bus_Routes.py
@@ -0,0 +1,11 @@
+for ii in range(int(input())):
+    n,d=map(int,input().split())
+    x=list(map(int,input().split()))
+    min=(10**12)+1
+    for jj in reversed(x):
+        dd=d//jj
+        r=dd*jj
+        d=r
+        if(min>r):
+            min=r
+    print("Case #" + str(ii + 1) + ": " + str(min))
